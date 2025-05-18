f6941a0d1de44902deacd706c3a8dd9dfbe8a221
Benjamin Connor Jones <bennycjonesmusic@gmail.com>
2025-05-18 22:34:28 +0100

    Working on search queries. Should now be able to search by keys, popularity, vocal ranges and date added. Needs bug checking

--- controllers/tracksController.js
+++ controllers/tracksController.js
@@ -1,5 +1,6 @@
 import { ListObjectsV2Command, S3Client } from '@aws-sdk/client-s3';
 import BackingTrack from '../models/backing_track.js';
+import { parseKeySignature } from './utils';  
 
 export const listS3 = async (req, res) => {
     const s3 = new S3Client({
@@ -29,34 +30,59 @@ export const listS3 = async (req, res) => {
 
 //controller for sorting tracks by popularity amongst other things
 export const queryTracks = async (req, res) => {
-
     try {
+        const { orderBy, page = 1, limit = 10, keySig, "vocal-range": vocalRange } = req.query; //destructuring the page. Because this is my first project, query is a way of getting data from the url. for example.. /tracks?page=1&... e.t.c
 
-        const {orderBy, page = 1, limit = 10} = req.query; //destructuring the page. Because this is my first project, query is a way of getting data from the url. for example.. /tracks?page=1&... e.t.c
+        let sort = {};  //define an empty object to hold the sort order
+        let filter = {}; // Object to hold the filtering options
 
-
-        let sort = {}; //define an empty object to hold the sort order
-
-        if(orderBy == "popularity"){
-
+        if (orderBy == "popularity") {
             sort = { purchaseCount: -1} //sort by purchase count. -1 for descending. we will add more options later.
         }
 
-        const tracks = await BackingTrack.find({}).sort(sort).skip((page - 1) * limit).limit(limit);
+        if (orderBy == "date-uploaded") {
+            sort = { createdAt: -1};
+        }
+
+        if (orderBy == "date-uploaded/ascending") {
+            sort = {createdAt: 1};
+        }
+        //consider arranging all these orderbys into one map
+
+        if (orderBy == "rating") {
+            sort = {averageRating: -1};
+        }
+
+        if (keySig) {
+            try {
+                const { key, isFlat, isSharp } = parseKeySignature(keySig);
+                filter.key = key;
+                if (isFlat) filter.isFlat = true;
+                if (isSharp) filter.isSharp = true;
+            } catch(error){
+                return res.status(400).json({error: error.message}) //may not pass correctly, check this
+            }
+        };
+
+        if (vocalRange) {
+            try {
+                filter.vocalRange = vocalRange;
+            } catch(error){
+                return res.status(400).json({error: "Something went wrong. Make sure you enter valid vocal range"});
+            }
+        };
+
+        const tracks = await BackingTrack.find(filter).sort(sort).skip((page - 1) * limit).limit(limit); //find tracks with query. Limit searches to ten per page. Make sure to have next-page functionality in front end.
         if (!tracks || tracks.length === 0) {
             return res.status(404).json({message: "No tracks found."}); 
         }
 
         res.status(200).json(tracks);
-    }catch(error) {
+    } catch(error) {
         res.status(500).json({ error: "Failed to query tracks" }); 
         console.error('Error querying tracks:', error);
     }
-
-
-
 };
-
 //controller for querying tracks on my website via name
 
 export const searchTracks = async (req, res) => {
--- models/backing_track.js
+++ models/backing_track.js
@@ -25,7 +25,35 @@ const backingTrackSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,  // ObjectId type for linking to another model
     ref: 'User', // This references the 'User' model
     required: true // Ensure that the track is always linked to a user
+  }, 
+  
+  //define key signature, will be used in querying.
+  key: {
+    enum: ["A", "B", "C", "D", "E", "F", "G"],
+    required: false,
+
+    
+
   },
+    isFlat: {
+    type: Boolean,
+    default: false,
+    required: false,
+  },
+
+  isSharp: {
+    type: Boolean,
+    default: false,
+    required: false,
+  },
+
+  vocalRange: {
+
+    type: String,
+    enum: ["Soprano", "Mezzo-Soprano", "Contralto", "Countertenor", "Tenor", "Baritone", "Bass;"],
+    required: false,
+  },
+ 
   genre: {
     type: String,
     required: false,
@@ -34,8 +62,6 @@ const backingTrackSchema = new mongoose.Schema({
     type: String,
     enum: ['yes', 'no'],
     default: 'no',
-
-
   },
   createdAt: {
     type: Date,
@@ -51,12 +77,59 @@ const backingTrackSchema = new mongoose.Schema({
     type: Number,
     default: 0,
   },
+  ratings: [
+{
+user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+stars: {type: Number, min: 1, max: 5, required: true}, 
+ratedAt: { type: Date, default: Date.now }
+
+// ratings with stars.
+  
+},
+  ],
+
+averageRating: {
+  type: Number,
+  default: 0
+} //compute average rating, function below
+
+  
   //reviews: [reviewSchema] //need to make review schema
 
 
 });
 
 backingTrackSchema.index({ title: 'text' });
+
+
+
+//method for average rating
+backingTrackSchema.methods.calculateAverageRating = function () {
+
+  if (this.ratings.length === 0){
+    this.averageRating = 0;
+    return;
+  }
+const total = this.ratings.reduce((sum, rating) => sum + rating.stars, 0); //use array method to get average.
+//sum is running total, and rating is current val. 0 is the starting value of sum in this instance
+
+this.averageRating = total / this.ratings.length; //normal way of getting average. total divided by num of items
+}
+
+backingTrackSchema.virtual('musicalKey').get(function() {
+if (this.isFlat && this.isSharp){
+
+    return "Cannot both be sharp and flat";//argument to stop both isFlat and isSharp existing
+  }
+  let keySignature = this.key;
+  if (this.isFlat) keySignature += "b";
+  if (this.isSharp) keySignature += "#";
+  
+
+  return keySignature;
+
+});
+
 
 // Create the model
 const BackingTrack = mongoose.model('BackingTrack', backingTrackSchema);
