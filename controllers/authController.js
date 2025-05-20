00d50eb2f3bf8aa5e8018d0cfbbfe39da41daa17
Benjamin Connor Jones <bennycjonesmusic@gmail.com>
2025-05-20 01:25:59 +0100

    Basic housekeeping. Organised things a bit better, and sorted a few bugs that arose out of the module implications.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,10 @@
 import fs from 'fs';
-import { S3Client } from '@aws-sdk/client-s3';
+import { S3Client, DeleteObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
 import { Upload } from '@aws-sdk/lib-storage';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken'; //going to use in register as well, to authenticate email
 import User from '../models/User.js';
 import backingTrack from '../models/backing_track.js';
-import { DeleteObjectCommand } from '@aws-sdk/client-s3';
-import { GetObjectCommand } from '@aws-sdk/client-s3';
 import * as Filter from 'bad-words'; //package to prevent profanity
 import zxcvbn from 'zxcvbn'; //package for password strength
 import { validateEmail } from '../utils/emailValidator.js';
@@ -75,92 +73,8 @@ export const login = async (req, res) => {
     }
 };
 
-export const uploadTrack = async (req, res) => {
-    try {
-        if (!req.file) {
-            return res.status(400).json({ message: 'No file uploaded' });
-        }
-        const fileStream = fs.createReadStream(req.file.path);
-        const s3Client = new S3Client({
-            region: process.env.AWS_REGION,
-            credentials: {
-                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-            },
-        });
-        const uploadParams = {
-            Bucket: process.env.AWS_BUCKET_NAME,
-            Key: `songs/${Date.now()}-${req.file.originalname}`,
-            Body: fileStream,
-            ACL: 'private',
-        };
-        const data = await new Upload({
-            client: s3Client,
-            params: uploadParams,
-        }).done();
-        const newTrack = new backingTrack({
-            title: req.body.title || req.file.originalname,
-            description: req.body.description || 'No description provided',
-            fileUrl: data.Location,
-            s3Key: uploadParams.Key,
-            price: parseFloat(req.body.price) || 0,
-            user: req.userId,
-        });
-        await newTrack.save();
-        const updateUser = await User.findByIdAndUpdate(req.userId, { $push: { uploadedTracks: newTrack._id } }, { new: true });
-        if (!updateUser) {
-            return res.status(404).json({ message: "User not found." });
-        }
-        fs.unlinkSync(req.file.path);
-        res.status(200).json({ message: 'File uploaded successfully!', track: newTrack });
-    } catch (error) {
-        console.error('Error uploading backing track:', error);
-        res.status(500).json({ message: 'Internal server error' });
-    }
-};
 
-export const deleteTrack = async (req, res) => {
-    try {
-        const Track = await backingTrack.findById(req.params.id);
-        if (!Track) {
-            return res.status(404).json({ message: "Track not found." });
-        }
-        if (Track.user.toString() !== req.userId) {
-            return res.status(403).json({ message: "You are not authorized to delete this track." });
-        }
-        if (!Track.s3Key) {
-            return res.status(400).json({ message: "Track does not have an associated s3Key." });
-        }
-        const s3Client = new S3Client({
-            region: process.env.AWS_REGION,
-            credentials: {
-                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-            },
-        });
-        const deleteParameters = {
-            Bucket: process.env.AWS_BUCKET_NAME,
-            Key: Track.s3Key,
-        };
-        await User.findByIdAndUpdate(req.userId, { $pull: { uploadedTracks: req.params.id } }, { new: true });
-        await s3Client.send(new DeleteObjectCommand(deleteParameters));
-        await backingTrack.findByIdAndDelete(req.params.id);
-        res.status(200).json({ message: 'Track and file deleted' });
-    } catch (error) {
-        console.error('There was an error deleting track:', error);
-        res.status(500).json({ message: 'Internal server error' });
-    }
-};
 
-export const getTracks = async (req, res) => {
-    try {
-        const tracks = await backingTrack.find({ user: req.userId }).sort({ createdAt: -1 });
-        res.status(200).json(tracks);
-    } catch (error) {
-        console.error('Error fetching tracks:', error);
-        res.status(500).json({ message: 'Internal server error' });
-    }
-};
 
 export const updateS3Key = async (req, res) => {
     try {
@@ -182,64 +96,3 @@ export const updateS3Key = async (req, res) => {
         res.status(500).json({ message: 'Internal server error' });
     }
 };
-
-export const downloadTrack = async (req, res) => {
-
-try {
-
-    const track = await backingTrack.findById(req.params.id);
-    if (!track) {
-        return res.status(404).json({message: "Track not found."});
-    }
-
-    const userId = req.userId;
-
-    const user = await User.findById(userId); //find the user wanting to download track
-
-    const hasBought = user.boughtTracks.some(id => id.equals(track._id)); //had to use .some so we can access the .equals method. .includes used strict equality === which is not correct here.
-    const hasUploaded = user.uploadedTracks.some(id => id.equals(track._id));
-
-    if (!hasBought && !hasUploaded){
-    
-        return res.status(403).json({message: "You are not allowed to download this track. Please purchase"})
-    }
-
-
-     const s3Client = new S3Client({
-            region: process.env.AWS_REGION,
-            credentials: {
-                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-            },
-        });
-
-          const createParameters = {
-            Bucket: process.env.AWS_BUCKET_NAME,
-            Key: track.s3Key,
-        };
-
-        const command = new GetObjectCommand(createParameters);
-        const data = await s3Client.send(command);
-        track.downloadCount += 1;
-        await track.save();
-
-        res.setHeader('Content-Type', data.ContentType);
-res.setHeader('Content-Disposition', `attachment; filename="${track.title}"`);
-
-data.Body.pipe(res);
-
-    
-} catch (error) {
-
-    console.error('Error downloading track:', error);
-    res.status(500).json({ message: 'Internal server error' });
-
-
-
-}
-
-
-
-
-
-}
--- routes/tracks.js
+++ routes/tracks.js
@@ -1,6 +1,11 @@
 import express from 'express';
 
-import { listS3, queryTracks, searchTracks } from '../controllers/tracksController.js';
+import upload from '../middleware/song_upload.js';
+import authMiddleware from '../middleware/customer_auth.js';
+import artistAuthMiddleware from '../middleware/artist_auth.js';
+import {uploadLimiter, registerLimiter} from '../middleware/rate_limiter.js' 
+
+import { listS3, queryTracks, searchTracks, downloadTrack, getTracks, deleteTrack, uploadTrack} from '../controllers/tracksController.js';
 
 const router = express.Router();
 
@@ -10,3 +15,14 @@ router.get('/queryTracks', queryTracks); //needs no auth middleware as it is pub
 
 router.get('/searchTracks', searchTracks); //no need for camelcase here. search-tracks = standard practice. change it.
 export default router;
+
+//now we handle the upload of backing tracks. Create, Read, Update and Delete Operations. For now though, create and delete will suffice.
+
+router.post('/upload', authMiddleware, uploadLimiter, upload.single('file'), uploadTrack)
+
+router.delete('/delete/:id', authMiddleware, deleteTrack)
+
+//get tracks from the user. This will be used to display the tracks on the front end.
+router.get('/tracks', authMiddleware, getTracks)
+
+router.get('/downloadTrack/:id', authMiddleware, downloadTrack) //req.params.id = :id needed in route
