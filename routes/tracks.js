71f9dce9c3c996465a461584d83ef353e2b08ee1
Benjamin Connor Jones <bennycjonesmusic@gmail.com>
2025-05-20 15:25:25 +0100

    Added previous changes to routes. ready to test

--- routes/tracks.js
+++ routes/tracks.js
@@ -3,26 +3,39 @@ import express from 'express';
 import upload from '../middleware/song_upload.js';
 import authMiddleware from '../middleware/customer_auth.js';
 import artistAuthMiddleware from '../middleware/artist_auth.js';
-import {uploadLimiter, registerLimiter} from '../middleware/rate_limiter.js' 
+import { uploadLimiter, registerLimiter } from '../middleware/rate_limiter.js' //limit amount of times someone can upload
 
-import { listS3, queryTracks, searchTracks, downloadTrack, getTracks, deleteTrack, uploadTrack} from '../controllers/tracksController.js';
+import {
+  listS3,
+  queryTracks,
+  searchTracks,
+  downloadTrack,
+  getTracks,
+  deleteTrack,
+  uploadTrack,
+  getTrack
+} from '../controllers/tracksController.js';
 
 const router = express.Router();
 
-router.get('/listS3', listS3);
+router.get('/tracks/list-s3', listS3);
 
-router.get('/queryTracks', queryTracks); //needs no auth middleware as it is public route.
+router.get('/tracks/query', queryTracks); //needs no auth middleware as it is public route.
 
-router.get('/searchTracks', searchTracks); //no need for camelcase here. search-tracks = standard practice. change it.
-export default router;
+router.get('/tracks/search', searchTracks); //no need for camelcase here. search-tracks = standard practice. change it.
 
 //now we handle the upload of backing tracks. Create, Read, Update and Delete Operations. For now though, create and delete will suffice.
 
-router.post('/upload', authMiddleware, uploadLimiter, upload.single('file'), uploadTrack)
+router.post('/tracks/upload', authMiddleware, uploadLimiter, upload.single('file'), uploadTrack);
 
-router.delete('/delete/:id', authMiddleware, deleteTrack)
+router.delete('/tracks/:id', authMiddleware, deleteTrack); //delete a track by id
 
 //get tracks from the user. This will be used to display the tracks on the front end.
-router.get('/tracks', authMiddleware, getTracks)
+router.get('/tracks', authMiddleware, getTracks);
 
-router.get('/downloadTrack/:id', authMiddleware, downloadTrack) //req.params.id = :id needed in route
+//get one singular track
+router.get('/tracks/:id', authMiddleware, getTrack);
+
+router.get('/tracks/download/:id', authMiddleware, downloadTrack); //req.params.id = :id needed in route
+
+export default router;
