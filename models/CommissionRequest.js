262bd1427946f21af1ff1f4e21fc645b391b9061
Benjamin Connor Jones <bennycjonesmusic@gmail.com>
2025-05-31 16:59:08 +0100

    Added guide track url for commissions

--- models/CommissionRequest.js
+++ models/CommissionRequest.js
@@ -15,6 +15,7 @@ const commissionRequestSchema = new mongoose.Schema({
   stripeTransferId: { type: String }, // <-- Add this line
   finishedTrackUrl: { type: String },
   previewTrackUrl: { type: String },
+  guideTrackUrl: { type: String }, //optional guide track url
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
   expiryDate: { type: Date, required: false }, // Optional deadline for delivery
